d9df6bac646cb33f31733450bdf1c51209a2ad1b
Killian.G <145963835+GURREA-Killian-23005142@users.noreply.github.com>
2024-09-18 21:36:44 +0200

    ajout de la classe database et mise a jour de user

---
+++ _assets/includes/database.php
@@ -0,0 +1,15 @@
+<?php
+    class database{
+        private $host = 'mysql-lecuyer-theotime-2326067l.alwaysdata.net';
+        private $dbName = 'lecuyer-theotime-2326067l_tenrac';
+        private $userName = 'Killian_GURREA';
+        private $password = '1234';
+
+        public $conn;
+
+        //connexion à la base de données
+        public function getConnection(){
+            $this->conn;
+        }
+    }
+?>
--- _assets/includes/user.php
+++ _assets/includes/user.php
@@ -19,7 +19,7 @@
         //Fonction pour creer un nouvel utilisateur
         public function createNewUser(){
             $query = "INSERT INTO " . $this->table . "(userName, userEmail, userPassword, userGender, userGrade) 
-            VALUES(:nameUser, :emailUser, :passwordUser,:genderUser, :GradeUser)";
+            VALUES(:userName, :userEmail, :userPassword,:userGender, :userGrade)";
 
             $stmt = $this->conn->prepare($query);
 
@@ -31,11 +31,11 @@
             $this->userGrade = htmlspecialchars(strip_tags($this->userGrade));
 
             //lier les paramètres
-            $stmt->bindParam(':nameUser', $this->userName);
-            $stmt->bindParam(':emailUser', $this->userEmail);
-            $stmt->bindParam(':passwordUser', $this->userPassword);
-            $stmt->bindParam(':genderUser', $this->userGender);
-            $stmt->bindParam(':GradeUser', $this->userGrade);
+            $stmt->bindParam(':userName', $this->userName);
+            $stmt->bindParam(':userEmail', $this->userEmail);
+            $stmt->bindParam(':userPassword', $this->userPassword);
+            $stmt->bindParam(':userGender', $this->userGender);
+            $stmt->bindParam(':userGrade', $this->userGrade);
 
             // exécuter la requête
             if($stmt->execute()){
@@ -44,17 +44,83 @@
             return false;
         }
 
+        // fonction pour lire tous les utilisateurs
+        public function read(){
+            $query = "SELECT userID, userName, userEmail, userPassword, userGender, userGrade
+                      FROM " . $this->table;
+            
+            $stmt = $this->conn->prepare($query);
+            $stmt->execute();
+
+            return $stmt;
+        }
+
+        // fonction pour lire un utilisateur par ID
+        public function readOneById(){
+            $query = "SELECT userID, userName, userEmail, userPassword, userGender, userGrade
+                      FROM " . $this->table . "WHERE userID = ? LIMIT 0,1";
+            
+            $stmt = $this->conn>prepare($query);
+            $stmt->bindParam(1, $this->userID);
+            $stmt->execute();
+
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if($row){
+                //assigner les valeurs au propriété
+                $this->userName = $row['userName'];
+                $this->userEmail = $row['userEmail'];
+                $this->userPassword = $row['userPassword'];
+                $this->userGender = $row['userGender'];
+                $this->userGrade = $row['userGrade'];
+            }
+        }
+
+        public function updateUser(){
+            $query = "UPDATE " . $thiss->table . "
+                      SET userName = :userName,
+                          userEmail = :userEmail
+                          userPassword = :userPassword
+                          userGender = :userGender
+                          userGrade = :userGrade
+                      WHERE userID = :userID";
+            
+            $stmt = $this->conn->prepare($query);
+
+            //nettoyer les données
+            $this->userName = htmlspecialchars(strip_tags($this->userName));
+            $this->userEmail = htmlspecialchars(strip_tags($this->userEmail));
+            $this->userPassword = htmlspecialchars(strip_tags($this->userPassword));
+            $this->userGender = htmlspecialchars(strip_tags($this->userGender));
+            $this->userGrade = htmlspecialchars(strip_tags($this->userGrade));
+            $this->userID = htmlspecialchars(strip_tags($this->userID));
+
+            //lier les parametres
+            $stmt->bindParam(':userName', $this->userName);
+            $stmt->bindParam(':userEmail', $this->userEmail);
+            $stmt->bindParam(':userPassword', $this->userPassword);
+            $stmt->bindParam(':userID', $this->userID);
+            $stmt->bindParam(':userGender', $this->userGender);
+            $stmt->bindParam(':userGrade', $this->userGrade);
+
+            //executer la requete
+            if ($stmt->execute()){
+                return true;
+            }
+            return false;
+        }
+
         // suprimer un utilisateur
         public function deleteUser(){
-            $query = "DELETE FROM " . $this->table . "WHERE userId = :idUser";
+            $query = "DELETE FROM " . $this->table . "WHERE userID = :userID";
 
             $stmt = $this->conn->prepare($query);
 
             // nettoyer id
-            $this->userId = htmlspecialchars(strip_tags($this->userId));
+            $this->userID = htmlspecialchars(strip_tags($this->userID));
 
             // id a suprimer
-            $stmt->bindParam(':idUser', $this->userID);
+            $stmt->bindParam(':userID', $this->userID);
 
             // lancer la requête
             if($stmt->execute){
