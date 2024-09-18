11403ac8c1cc1edba509583ca12e52b15e9c4325
Bol2riz <maxime.allasio@etu.univ-amu.fr>
2024-09-18 12:13:41 +0200

    Modification de la connexion à la base de données

--- modules/index.php
+++ modules/index.php
@@ -1,20 +1,21 @@
 <?php
 
 $user = '343207';
-$pass = 'tenraczebi';
+$pwd = 'tenraczebi';
 
 
 try {
     // essaye de se connecter à la base de données (BD)
-    $connection = new PDO ('mysql:host=$host; dbname=User', $user, $pwd);
+    $connection = new PDO ('mysql:host=mysql-lecuyer-theotime-2326067l.alwaysdata.net;dbname=lecuyer-theotime-2326067l_tenrac', $user, $pwd);
     // Selectionne la ligne userID de la BD et l'affiche
-    foreach ($connection->query('SELECT * FROM userID') as $key) {
+    foreach ($connection->query('SHOW TABLES') as $row) {
         print_r($row);
     }
-} 
+}
 // Si échoue à se connecté à la BD
 catch (PDOException $e) {
     // Affiche un message d'erreur
-    print "Erreur :" . -$e->getMessage() . "<br/r>";
+    print "Erreur :" . $e->getMessage() . "<br/r>";
     die;
 }
+?>
