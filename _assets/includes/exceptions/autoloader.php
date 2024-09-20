e2146ab1f3f294239c8988bba8105616d569dc12
Alex Goncalves Rodrigues <alex.GONCALVES-RODRIGUES@etu.univ-amu.fr>
2024-09-20 16:12:53 +0200

    Mise en place de quelques commentaires

--- _assets/includes/exceptions/autoloader.php
+++ _assets/includes/exceptions/autoloader.php
@@ -6,20 +6,20 @@ final class autoloader //fonction pour les chargements
 {
     public static function loadClassCore ($S_nameClasse) // fonction pour charger  le noyaux, le S_ est pour dire qu'elle est static
     {
-        $S_file = constants::directoryCore() . "$S_nameClasse.php";
-        return static::_load($S_file);
+        $S_file = constants::directoryCore() . "$S_nameClasse.php";   // Construit le chemin complet vers la classe du noyau à partir du nom de classe
+        return static::_load($S_file); // Appelle la méthode privée _load pour inclure le fichier
     }
 
-    public static function loadClassesException ($S_nameClasse)
+    public static function loadClassesException ($S_nameClasse) // Méthode pour charger les classes
     {
-        $S_file = constants::directoryExceptions() . "$S_nameClasse.php";
+        $S_file = constants::directoryExceptions() . "$S_nameClasse.php"; // Construit le chemin complet vers la classe d'exception à partir du nom de classe
 
         return static::_load($S_file);
     }
 
     public static function loadClassesModels ($S_nameClasse)
     {
-        $S_file = constants::directoryModel() . "$S_nameClasse.php";
+        $S_file = constants::directoryModel() . "$S_nameClasse.php"; // Construit le chemin complet vers la classe model à partir du nom de classe
 
         return static::_load($S_file);
     }
@@ -27,30 +27,30 @@ final class autoloader //fonction pour les chargements
 
     public static function loadClassesViews ($S_nameClasse)
     {
-        $S_file = constants::directoryViews() . "$S_nameClasse.php";
+        $S_file = constants::directoryViews() . "$S_nameClasse.php";// Construit le chemin complet vers la classe views à partir du nom de classe
 
         return static::_load($S_file);
     }
 
     public static function loadClassesControllers ($S_nameClasse)
     {
-        $S_file = constants::directoryControl() . "$S_nameClasse.php";
+        $S_file = constants::directoryControl() . "$S_nameClasse.php"; // Construit le chemin complet vers la classe controlleurs à partir du nom de classe
 
         return static::_load($S_file);
     }
 
-    private static function _load ($S_fileToLoad)
+    private static function _load ($S_fileToLoad)  // Méthode privée qui fait réellement l'inclusion des fichiers
     {
-        if (is_readable($S_fileToLoad))
+        if (is_readable($S_fileToLoad)) // Vérifie si le fichier est lisible et existant avant de l'inclure
         {
-            require $S_fileToLoad;
+            require $S_fileToLoad; // Inclus le fichier de la classe correspondante
         }
     }
 }
 
-// J'empile tout ce beau monde comme j'ai toujours appris à le faire...
-spl_autoload_register('autoloader::loadClassCore');
-spl_autoload_register('autoloader::loadClassesException');
-spl_autoload_register('autoloader::loadClassesModels');
-spl_autoload_register('autoloader::loadClassesViews');
-spl_autoload_register('autoloader::loadClassesControllers');
+// Enregistre les méthodes de chargement automatique pour les différentes catégories de classes
+spl_autoload_register('autoloader::loadClassCore'); // Charge les classes du noyau
+spl_autoload_register('autoloader::loadClassesException'); // Charge les classes d'exception
+spl_autoload_register('autoloader::loadClassesModels'); // Charge les classes de model
+spl_autoload_register('autoloader::loadClassesViews'); // Charge les classes de views
+spl_autoload_register('autoloader::loadClassesControllers'); // Charge les classes de Controllers
--- modules/index.php
+++ modules/index.php
@@ -3,16 +3,17 @@
 require '_assets/includes/autoloader';
 
 try {
-    if (filter_input(INPUT_GET, 'action')) {
-        if ($_GET['action'] === 'post') {
-            if (filter_input(INPUT_GET, 'id') && $_GET['id'] > 0) {
-                (new \modules\blog\controllers\post())->execute($_GET['id']);
+    if (filter_input(INPUT_GET, 'action')) { // Vérifie si une action a été transmise via l'URL en utilisant la méthode GET
+        if ($_GET['action'] === 'post') {  // Si l'action est 'post', l'application doit afficher un article de blog spécifique
+            if (filter_input(INPUT_GET, 'id') && $_GET['id'] > 0) {   // Vérifie si un identifiant 'id' a été passé via l'URL et si cet identifiant est supérieur à 0
+                (new \modules\blog\controllers\post())->execute($_GET['id']); // Si un ID valide est fourni, on appelle le contrôleur 'post' et on exécute l'action avec l'ID
             }
-        throw new ControllerException('Aucun identifiant de billet envoyé');
+            throw new ControllerException('Aucun identifiant de billet envoyé'); // Si l'ID n'est pas fourni ou invalide, on lève une exception pour indiquer qu'aucun billet n'a été envoyé
         }
-    throw new ControllerException('La page que vous recherchez n\'existe pas');
+        throw new ControllerException('La page que vous recherchez n\'existe pas'); // Si l'action transmise via l'URL n'est pas reconnue (autre que 'post'), on lève une exception pour indiquer
+        // que la page n'existe pas
     }
-    (new \modules\blog\controllers\homepage())->execute();
+    (new \modules\blog\controllers\homepage())->execute(); // Si aucune action n'est spécifiée dans l'URL, exécute la page d'accueil du blog
 } catch (ControllerException $e) {
-    (new \modules\blog\views\error($e->getMessage()))->show();
+    (new \modules\blog\views\error($e->getMessage()))->show(); // Si une exception de type 'ControllerException' est attrapée, on affiche une page d'erreur avec le message correspondant
 }
