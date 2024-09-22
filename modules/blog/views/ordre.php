41d23a6cf4a922a553cebaf6f13ad4d0c280bfae
GUILLERM-Mathis-23016147 <mathis.guillerm@etu.univ-amu.fr>
2024-09-22 14:42:31 +0200

    modif ordre

---
+++ modules/blog/views/ordre.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTMl>
+<html lang="fr">
+<title>Ordre</title>
+<link rel="stylesheet" href="css/ordre.css">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<header>
+    <?php include 'header.php'?>
+</header>
+<body>
+
+</body>
+</html>
