5a9884c69fa6f815a06fc0565c37a31eb2f46db8
GUILLERM-Mathis-23016147 <mathis.guillerm@etu.univ-amu.fr>
2024-09-22 14:33:59 +0200

    connexioncss et php

---
+++ modules/blog/views/connexion.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTMl>
+<html lang="fr">
+<title>Connexion</title>
+<link rel="stylesheet" href="connexion.css">
+<?php include 'header.php'?>
+<body>
+<form method="post" action="connexion.php" id="connect" class="connect">
+    <div class="mail">
+        <libellé>
+            Email :
+        </libellé>
+        <input type="email" name="email" class="inpo" required> <br>
+    </div>
+    <div class="mdp">
+        <libellé>
+            Mot de Passe :
+        </libellé>
+        <input type="password" name="password" class="inpo" required> <br>
+    </div>
+<input type="submit" class="but" value="Connexion">
+</form>
+</body>
+</html>
