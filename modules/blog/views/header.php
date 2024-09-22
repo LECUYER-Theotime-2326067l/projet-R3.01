aa5f41cc23cde0f217a7bb267c94f52001586587
GUILLERM-Mathis-23016147 <mathis.guillerm@etu.univ-amu.fr>
2024-09-22 18:04:05 +0200

    header

--- modules/blog/views/header.php
+++ modules/blog/views/header.php
@@ -1,12 +1,30 @@
-<link rel="stylesheet" href="css/header.css">
+<?php
+session_start();
+function headerPage($page_title = "Titre par Défaut", $css_file = "Titre par défaut")
+{
+?>
+    <!DOCTYPE HTMl>
+    <html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/<?php echo $css_file; ?>">
+
+    <title><?php echo $page_title; ?></title>
+</head>
+<body>
 <header>
-    <select id="pages" class="pages">
-        <option value="accueil">Accueil</option>
-        <option value="structure">Structure</option>
-        <option value="repas">Repas</option>
-        <option value="plat">Plat</option>
-    </select>
-    <button class="connection">
+    <a href="" class="btn">acceuil</a>
+    <a href="" class="btn">repas</a>
+    <a href="" class="btn">structure</a>
+    <a href="" class="btn">plat</a>
+    <button class="btn">
         +
     </button>
 </header>
+</body>
+
+</html>
+<?php
+}
+?>
