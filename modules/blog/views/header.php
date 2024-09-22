dab86f5bee698951b83be1c99f4a846a3a6ffc54
GUILLERM-Mathis-23016147 <mathis.guillerm@etu.univ-amu.fr>
2024-09-22 14:42:18 +0200

    modif header

---
+++ modules/blog/views/header.php
@@ -0,0 +1,12 @@
+<link rel="stylesheet" href="css/header.css">
+<header>
+    <select id="pages" class="pages">
+        <option value="accueil">Accueil</option>
+        <option value="structure">Structure</option>
+        <option value="repas">Repas</option>
+        <option value="plat">Plat</option>
+    </select>
+    <button class="connection">
+        +
+    </button>
+</header>
