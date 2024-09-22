2f4534cfc44f78340a9e33096f22af44098a2660
GUILLERM-Mathis-23016147 <mathis.guillerm@etu.univ-amu.fr>
2024-09-22 14:34:23 +0200

    ordre et repas

---
+++ modules/blog/views/Repas.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTMl>
+<html lang="fr">
+<title>Repas</title>
+<link rel="stylesheet" href="Repas.css">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<?php include 'header.php'?>
+<body>
+<h1>
+    Repas
+</h1>
+    <div class="box">
+        Adresse : <br>
+        Date : <br>
+        Gérant : <br>
+    </div>
+    <div class="box">
+        list2
+    </div>
+    <div class="box">
+        list3
+    </div>
+</body>
+</html>
+<?php
