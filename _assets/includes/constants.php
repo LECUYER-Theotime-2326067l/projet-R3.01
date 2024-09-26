a9c5ec19dc43a1e84790b9f28d8ada0e533ff92d
Alex Goncalves Rodrigues <144851148+GONCALVES-RODRIGUES-Alex-2326052@users.noreply.github.com>
2024-09-26 09:10:20 +0200

    base home page pour test

---
+++ _assets/includes/constants.php
@@ -0,0 +1,36 @@
+<?php
+
+final class Constants
+{
+    // Chemins relatifs
+    const DIR_VIEWS        = '/modules/blog/views/';
+    const DIR_MODELS       = '/modules/blog/models/';
+    const DIR_CONTROLLERS  = '/modules/blog/controllers/';
+    const DIR_ASSETS       = '/_assets/';
+    const DIR_EXCEPTIONS   = '/_assets/includes/exceptions/';
+
+    // Méthodes pour obtenir les chemins complets
+    public static function directoryRoot() {
+        return realpath(__DIR__ . '/../../../'); // Chemin vers le répertoire racine
+    }
+
+    public static function directoryViews() {
+        return self::directoryRoot() . self::DIR_VIEWS;
+    }
+
+    public static function directoryModels() {
+        return self::directoryRoot() . self::DIR_MODELS;
+    }
+
+    public static function directoryControllers() {
+        return self::directoryRoot() . self::DIR_CONTROLLERS;
+    }
+
+    public static function directoryAssets() {
+        return self::directoryRoot() . self::DIR_ASSETS;
+    }
+
+    public static function directoryExceptions() {
+        return self::directoryRoot() . self::DIR_EXCEPTIONS;
+    }
+}
---
+++ modules/index.php
@@ -0,0 +1,24 @@
+<?php
+require __DIR__ . '/../_assets/includes/autoloader.php';
+require __DIR__ . '/../modules/blog/controllers/homepage.php';
+
+try {
+    if (filter_input(INPUT_GET, 'action')) {
+        if ($_GET['action'] === 'homepage') {
+            if (filter_input(INPUT_GET, 'id') && $_GET['id'] > 0) {
+                (new \modules\blog\controllers\homepage())->execute($_GET['id']);
+            }
+            throw new ControllerException('Aucun identifiant de billet envoyé');
+        }
+        if ($_GET['action'] === 'post'){
+            if (filter_input(INPUT_GET, 'id') && $_GET['id'] > 0) {
+                (new \modules\blog\controllers\PostController())->execute($_GET['id']);
+            } 
+            throw new ControllerException('Aucun identifiant de billet envoyé');
+        }
+        throw new ControllerException('La page que vous recherchez n\'existe pas');
+    }
+    (new \modules\blog\controllers\homepage())->execute();
+} catch (ControllerException $e) {
+    (new \modules\blog\views\error($e->getMessage()))->show();
+}
