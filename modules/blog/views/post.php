6d4ff7320e2b3ed821952472f132ae75707b6b93
GUILLERM-Mathis-23016147 <mathis.guillerm@etu.univ-amu.fr>
2024-09-28 19:37:07 +0200

    post correction css et php

--- modules/blog/views/post.php
+++ modules/blog/views/post.php
@@ -1,10 +1,11 @@
 <?php
 
-$css_file = "repas.css";
+$css_file = "post.css";
 include __DIR__ . '/header.php';
-headerPage("repas", $css_file);
+headerPage("Post", $css_file);
 ?>
 <meta name="description" content="Cette page liste les repas de l'ordre des tenrac">
+<main>
 <h1>
     Les Post du jour !
 </h1>
@@ -19,5 +20,5 @@ headerPage("repas", $css_file);
     <div class="box">
         list3
     </div>
-
+</main>
 <?php include 'footer.php'?>
