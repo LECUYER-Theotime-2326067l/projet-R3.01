3809ab14f96bce51b6bf74a82d615f67a0deca3c
GUILLERM-Mathis-23016147 <mathis.guillerm@etu.univ-amu.fr>
2024-10-01 19:54:11 +0200

    Update profil.php

--- modules/blog/views/profil.php
+++ modules/blog/views/profil.php
@@ -2,6 +2,9 @@
 $css_file = "profil.css";
 include constants::directoryViews() . '/header.php';
 headerPage("Profil", $css_file);
+$isLoggedIn = isset($_SESSION['user']);
+$userName = $isLoggedIn ? $_SESSION['user']['name'] : null;
+$userID = $isLoggedIn ? $_SESSION['user']['ID'] : null;
 ?>
 <meta name="description" content="Cette page est le profil utilisateur">
 <main>
@@ -10,8 +13,8 @@ headerPage("Profil", $css_file);
         Votre profil :
     </h1>
     <h2>
-        <?php echo htmlspecialchars($_SESSION['user']['userName']); ?> <br>
-        <?php echo htmlspecialchars($_SESSION['user']['userID']); ?>
+        <?php echo htmlspecialchars($userName); ?> <br>
+        <?php echo htmlspecialchars($userID); ?>
     </h2>
     </div>
 </main>
